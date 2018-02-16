39413a303d8edf25c005a7697c1adfedb6eaa39e
Joern Ott <joern.ott@ott-consult.de>
2018-02-16 10:42:35 +0000

    Initial commit

--- src/tmp/install/oc-rsyslog.sh
+++ src/tmp/install/oc-rsyslog.sh
@@ -4,9 +4,45 @@ set -x
 chmod a+rwx /tmp
 source /tmp/install/functions.sh
 
-install_software rsyslog
-sed -i /etc/rsyslog.conf \
-    -e 's|#$ModLoad imudp|$ModLoad imudp|' -e 's|#$UDPServerRun 514|$UDPServerRun 514|' \
-    -e 's|#$ModLoad imtcp|$ModLoad imtcp|' -e 's|#$InputTCPServerRun 514|$InputTCPServerRun 514|'
+install_software rsyslog logrotate
+
+cat >/ect/rsyslog.conf <<EOF
+# rsyslog configuration file
+
+#### MODULES ####
+
+$ModLoad imuxsock # provides support for local system logging (e.g. via logger command)
+$ModLoad imjournal # provides access to the systemd journal
+#$ModLoad immark  # provides --MARK-- message capability
+
+$ModLoad imudp
+$UDPServerRun 514
+$ModLoad imtcp
+$InputTCPServerRun 514
+
+#### GLOBAL DIRECTIVES ####
+$WorkDirectory /var/lib/rsyslog
+$ActionFileDefaultTemplate RSYSLOG_TraditionalFileFormat
+#$ActionFileEnableSync on
+$IncludeConfig /etc/rsyslog.d/*.conf
+$OmitLocalLogging on
+$IMJournalStateFile imjournal.state
+
+#### RULES ####
+$template perHostMessages,"/var/log/%HOSTNAME%/messages"
+*.info;mail.none;authpriv.none;cron.none -?perHostMessages
+
+$template perHostSecure,"/var/log/%HOSTNAME%/secure"
+authpriv.* -?perHostSecure
+
+$template perHostMaillog,"/var/log/%HOSTNAME%/maillog"
+mail.* -?perHostMaillog
+
+$template perHostCron,"/var/log/%HOSTNAME%/cron"
+cron.* -?perHostCron
+
+EOF
+
+
 
 cleanup
